d544e4f961fb5820e500385f03836b1b63955dda
hemalatha26407-eng <hemalatha26407@gmail.com>
2025-12-07 13:31:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 function addPost() {
     let input = document.getElementById("postInput");
     let text = input.value.trim();
-
     if (text === "") return;
-
     let list = document.getElementById("postList");
     let li = document.createElement("li");
     li.textContent = text;
-
     list.appendChild(li);
     input.value = "";
 }
